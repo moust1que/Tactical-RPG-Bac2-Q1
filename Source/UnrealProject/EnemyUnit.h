647cce055f69bb18c00d7de72366a63c9e829224
moust1que <emiliensalpeteur@gmail.com>
2024-11-29 21:59:51 +0100

    Creating new class and map
    - Add L_MainLevel
    - Set L_MainLevel in project settings
    - Add BaseCharacter class
    - Add PlayerUnit class as child of BaseCharacter
    - Add EnemyUnit class as child of BaseCharacter
    - Add Grid class

---
+++ Source/UnrealProject/EnemyUnit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "EnemyUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API AEnemyUnit : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UnrealProject/PlayerUnit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "PlayerUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API APlayerUnit : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
